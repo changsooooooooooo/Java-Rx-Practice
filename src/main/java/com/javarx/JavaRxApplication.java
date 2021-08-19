835c7b24150fcce8e3b3d54bfe83d6fd08056424
changsooooooooooo <lcs940621@gmail.com>
2021-08-19 14:59:26 +0900

    initial commit

---
+++ src/main/java/com/javarx/JavaRxApplication.java
@@ -0,0 +1,13 @@
+package com.javarx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaRxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaRxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javarx/JavaRxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javarx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaRxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
