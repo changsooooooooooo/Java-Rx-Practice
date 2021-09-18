388914f48eec536171aeb38da7067e01c85b42d7
changsooooooooooo <lcs940621@gmail.com>
2021-09-18 11:04:35 +0900

    flux & mono

--- Rx-Java-2/src/main/java/com/rxjava2/RxJava2Application.java
+++ Rx-Java-2/src/main/java/com/rxjava2/RxJava2Application.java
@@ -2,12 +2,23 @@ package com.rxjava2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.LongStream;
 
 @SpringBootApplication
 public class RxJava2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RxJava2Application.class, args);
-	}
 
+		Mono.just(1l)
+			.map(x -> x*2)
+			.or(Mono.delay(Duration.ofMillis(100)))
+			.subscribe(System.out::println);
+	}
 }
