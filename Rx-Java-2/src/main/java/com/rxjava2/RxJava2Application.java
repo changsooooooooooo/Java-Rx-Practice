7918928f69a05c339e7e1428cd59f16b93b419c5
changsooooooooooo <lcs940621@gmail.com>
2021-09-18 10:31:39 +0900

    flux started

---
+++ Rx-Java-2/src/main/java/com/rxjava2/RxJava2Application.java
@@ -0,0 +1,13 @@
+package com.rxjava2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RxJava2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RxJava2Application.class, args);
+	}
+
+}
---
+++ Rx-Java-2/src/test/java/com/rxjava2/RxJava2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rxjava2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RxJava2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
