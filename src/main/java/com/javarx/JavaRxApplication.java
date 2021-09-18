e06970effd37242139fd5e9533428b40c6135f17
changsooooooooooo <lcs940621@gmail.com>
2021-09-18 10:18:59 +0900

    flux creation test

--- src/main/java/com/javarx/JavaRxApplication.java
+++ src/main/java/com/javarx/JavaRxApplication.java
@@ -2,12 +2,18 @@ package com.javarx;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.core.publisher.Flux;
 
 @SpringBootApplication
 public class JavaRxApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavaRxApplication.class, args);
+
+		Flux<String> flux = Flux.just("A");
+		Flux<String> flux2 = flux.map(s->"foo"+s);
+		flux.subscribe(x-> System.out.println(x));
+		flux2.subscribe(x-> System.out.println(x));
 	}
 
 }
